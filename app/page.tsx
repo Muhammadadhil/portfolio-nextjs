198d4829b22e78dc2b4e4eed881171a46c543b9f
muhammad adhil <muhammadadhil934@gmail.com>
2025-04-29 14:20:59 +0530

    solved bugs

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
     };
 
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col max-h-screen">
             <main className="flex-1">
                 <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
                     <div className="container px-4 md:px-6">
